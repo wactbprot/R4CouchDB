df43552e7b95891ab23b11f7009c00ec63e7c9d5
th <tg@linux-kwnr.berlin.ptb.de>
2009-08-08 15:00:44 +0200

    new file: couchConConv.R 	new file: getUuids.R

---
+++ couchConConv.R
@@ -0,0 +1,4 @@
+couchConConv <- list(
+                     serverName = "localhost",
+                     port = "5984",
+                     noOfUuids="1")
---
+++ getUuids.R
@@ -0,0 +1,10 @@
+getUuids <- function(couchConConv){
+# write test functions!
+  adrString <- paste("http://",
+                     couchConConv$serverName,":",
+                     couchConConv$port, "/_uuids?count=",
+                     couchConConv$noOfUuids,
+                     sep="")
+ res <- getURL(adrString)
+  return( fromJson(res))
+}
