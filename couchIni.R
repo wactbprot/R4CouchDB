6ef67288896a020bd18e9bce29cf5554f1495e85
wactbprot <thsteinbock@web.de>
2009-08-09 12:16:02 -0700

    add comments

--- couchIni.R
+++ couchIni.R
@@ -2,16 +2,17 @@ rc <- library(RCurl, logical.return = TRUE)
 rj <- library(rjson, logical.return = TRUE)
 
 if(rc && rj){
-  print("--------------------------------")
-  print("       libs loaded              ")
-  print("--------------------------------")
+
+  print("\nlibs loaded\n")
 
         couchConConv <- list(
                      serverName = "localhost",
                      port = "5984",
-                     noOfUuids="1"
+                     noOfUuids="1",
                      )
 
   source("getUuids.R")
 
+}else{
+## libs can be loaded online ...
 }
