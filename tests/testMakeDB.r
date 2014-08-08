9e1938fc880b1ba0b9f03ebd41ea2eecbdfc8a0c
wactbprot <thsteinbock@web.de>
2014-08-08 10:40:27 +0200

    .. testMakeDB

--- tests/testMakeDB.r
+++ tests/testMakeDB.r
@@ -4,30 +4,35 @@ context("testing cdbMakeDB()")
 cdb <- cdbIni()
 
 test_that("error msg generated", {
-
     expect_that(cdbMakeDB(cdb), throws_error(
         "^.*no cdb\\$newDBName given"
         ))
-
 })
 
 
-test_that("base functionality of cdbAddDoc()", {
+test_that("base functionality of cdbMakeDB()", {
+
     dbs  <- unlist(cdbListDB(cdb)$res)
     i    <- which(dbs %in% c(testConsts$db))
-    
-    
     if(length(i) > 0){
         cdb$removeDBName <- testConsts$db
-        tmp <- cdbRemoveDB(cdb)
+        tmp              <- cdbRemoveDB(cdb)
     }
 
     cdb$newDBName <- testConsts$db
-    cdb <- cdbMakeDB(cdb)
+    cdb           <- cdbMakeDB(cdb)
     
-    expect_that(cdb$res$ok, equals(TRUE))
+    expect_that(cdb$res$ok,    equals(TRUE))
     expect_that(cdb$newDBName, equals(""))
-    expect_that(cdb$DBName, equals(testConsts$db))
-    
+    expect_that(cdb$DBName,    equals(testConsts$db))
 
+
+})
+
+test_that("error msg generated", {
+    cdb$newDBName <- testConsts$db
+
+    expect_that(cdbMakeDB(cdb), throws_error(
+        "^.*cdb\\$newDBName already exists"
+        ))
 })
